704c9e6d019935b04ee41b81d810aae8a08f2e52
Dávid Andor <david-andor@kozpontiagy.hu>
2019-12-30 13:09:44 +0100

    Upgrade dependencies

--- tests/src/bootstrap.php
+++ tests/src/bootstrap.php
@@ -6,6 +6,6 @@
  */
 
 require_once __DIR__ . '/../../vendor/autoload.php';
-require_once __DIR__ . '/../../vendor/weitzman/drupal-test-traits/src/bootstrap-fast.php';
+require_once __DIR__ . '/../../vendor/weitzman/drupal-test-traits/src/bootstrap.php';
 require_once __DIR__ . '/../fixtures/project_01/docroot/core/tests/bootstrap.php';
 require_once __DIR__ . '/../../vendor/drush/drush/includes/output.inc';
